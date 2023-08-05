9dd05892d61eeca2cdcaf8041bb624921e722e6f
Bethania La Manna <bethanialamanna@gmail.com>
2023-08-05 20:53:04 -0300

    quito metodo seter de id

--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -1,5 +1,4 @@
 package com.mindhub.homebanking.models;
-
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
@@ -20,11 +19,6 @@ public class Client {
     public Long getId() {
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
@@ -32,25 +26,24 @@ public class Client {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-public Client(){
-
-}
+    public Client(){
+    }
+    public Client(String firstName, String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
     @Override
     public String toString() {
         return "Client{" +
